8985e385eb01f155b374a7a5398cfee0333828df
ryan <ryanmarchock@gmail.com>
2017-02-23 13:26:34 +0000

    added types file and reworked initail state

--- src/reducers/shopping-list_reducer.js
+++ src/reducers/shopping-list_reducer.js
@@ -1,29 +1,7 @@
+import {SHOPPING_LIST_INITIAL_STATE} from './shopping_list_initial_state';
 import { UPDATE_SHOPPING_LIST } from '../actions/index';
 
-const INITIAL_STATE = { list: [
-  {
-    name: 'yellow or orange peppers',
-    amount: 2
-  },
-  {
-    name: 'tsp chipotle paste',
-    amount: '2'
-  },
-  {
-    name: 'red wine vinegar',
-    amount: '2 tbsp'
-  },
-  {
-    name: 'cocoa powder',
-    amount: '1 tbsp'
-  },
-  {
-    name: 'dried oregano',
-    amount: '1 tbsp'
-  }
-], item: null, edit: false };
-
-export default function(state = INITIAL_STATE, action) {
+export default function(state = SHOPPING_LIST_INITIAL_STATE, action) {
 
   switch (action.type) {
 
---
+++ src/reducers/shopping_list_initial_state.js
@@ -0,0 +1,22 @@
+export const SHOPPING_LIST_INITIAL_STATE = { list: [
+  {
+    name: 'yellow or orange peppers',
+    amount: 2
+  },
+  {
+    name: 'tsp chipotle paste',
+    amount: '2'
+  },
+  {
+    name: 'red wine vinegar',
+    amount: '2 tbsp'
+  },
+  {
+    name: 'cocoa powder',
+    amount: '1 tbsp'
+  },
+  {
+    name: 'dried oregano',
+    amount: '1 tbsp'
+  }
+], item: null, edit: false };
