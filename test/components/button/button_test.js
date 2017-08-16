d1a566a7a84802ed0150344dfc6c9f7eaa63f0ea
ryan <ryanmarchock@gmail.com>
2017-08-16 11:58:16 +0100

    button component test

---
+++ test/components/button/button_test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { renderComponent, expect } from '../../test_helper';
+import { mount } from 'enzyme'
+import { MemoryRouter } from 'react-router-dom';
+import Button from '../../../src/components/button/button';
+
+
+describe('<Button/>', function () {
+
+    let component;
+
+    beforeEach(() => {
+        component = mount(
+            <MemoryRouter>
+                <Button type="button" />
+            </MemoryRouter>
+        );
+    });
+
+    it('exists', function () {
+        expect(component.find('button').exists()).to.be.true;
+    });
+});
