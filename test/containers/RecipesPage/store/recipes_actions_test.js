b6ebd27f147de1f4484d5f211c6df09db3d6c5a4
ryan <ryanmarchock@gmail.com>
2017-08-18 20:34:49 +0100

    updated store tests

--- test/containers/RecipesPage/store/recipes_actions_test.js
+++ test/containers/RecipesPage/store/recipes_actions_test.js
@@ -1,93 +1,38 @@
-// import { expect } from '../../test_helper';
-//
-// import * as actions from '../../../src/components/RecipesPage/store/recipe.actions';
-// import * as types from '../../../src/components/RecipesPage/store/recipe.types';
-//
-// describe('Actions Recipes', () => {
-//   it('should create an action to update shopping list', () => {
-//     const expectedAction = {
-//       type: types.UPDATE_SHOPPING_LIST,
-//       payload: {}
-//     }
-//     expect(actions.updateShoppingList({})).to.eql(expectedAction)
-//   });
-//
-//   it('should create an action to update RecipesPage', () => {
-//     const expectedAction = {
-//       type: types.UPDATE_RECIPES,
-//       payload: {}
-//     }
-//     expect(actions.updateRecipes({})).to.eql(expectedAction)
-//   });
-//
-//   it('should create an action to fetch new recipe', () => {
-//     const expectedAction = {
-//       type: types.FETCH_NEW_RECIPE,
-//       payload: {}
-//     }
-//     expect(actions.fetchNewRecipe({})).to.eql(expectedAction)
-//   });
-//
-//   it('should create an action to save new recipe', () => {
-//     const expectedAction = {
-//       type: types.SAVE_NEW_RECIPE,
-//       payload: {}
-//     }
-//     expect(actions.saveNewRecipe({})).to.eql(expectedAction)
-//   });
-//
-//   it('should create an action to DELETE_RECIPE', () => {
-//
-//     const payload = {
-//       RecipesPage: [{name: '', discription: ''}, {name: '', discription: ''}],
-//       payload: 0
-//     }
-//
-//     const expectedAction = {
-//       type: types.DELETE_RECIPE,
-//       payload: [{name: '', discription: ''}],
-//     }
-//
-//     expect(actions.deleteRecipe(payload)).to.eql(expectedAction)
-//   });
-//
-//   it('should create an action to fetch recipe', () => {
-//     const expectedAction = {
-//       type: types.FETCH_RECIPE,
-//       payload: {}
-//     }
-//     expect(actions.fetchRecipe({})).to.eql(expectedAction)
-//   });
-//
-//   it('should create an action to save recipe', () => {
-//     const expectedAction = {
-//       type: types.SAVE_RECIPE,
-//       payload: {}
-//     }
-//     expect(actions.saveRecipe({})).to.eql(expectedAction)
-//   });
-//
-//   it('should create an action to clear recipe', () => {
-//     const expectedAction = {
-//       type: types.CLEAR_RECIPE,
-//       payload: {}
-//     }
-//     expect(actions.clearRecipe({})).to.eql(expectedAction)
-//   });
-//
-//   it('should create an action to delete ingredient', () => {
-//     const expectedAction = {
-//       type: types.DELETE_INGREDIENT,
-//       payload: {}
-//     }
-//     expect(actions.deleteIngredient({})).to.eql(expectedAction)
-//   });
-//
-//   it('should create an action to add ingredient', () => {
-//     const expectedAction = {
-//       type: types.ADD_INGREDIENT,
-//       payload: {}
-//     }
-//     expect(actions.addIngredient({})).to.eql(expectedAction)
-//   });
-// })
+import { expect } from '../../../test_helper';
+
+import * as actions from '../../../../src/containers/RecipesPage/store/recipe.actions';
+import * as types from '../../../../src/containers/RecipesPage/store/recipe.types';
+
+describe('Recipes Actions', () => {
+    it('should update a recipe', () => {
+        const expectedAction = {
+            type: types.RECIPE_UPDATE,
+            payload: {}
+        };
+        expect(actions.recipeUpdate({})).to.eql(expectedAction)
+    });
+
+    it('should create a new recipe', () => {
+        const expectedAction = {
+            type: types.RECIPE_NEW,
+            payload: {}
+        };
+        expect(actions.recipeNew({})).to.eql(expectedAction)
+    });
+
+    it('should select a new recipe', () => {
+        const expectedAction = {
+            type: types.RECIPE_SELECTED,
+            payload: {}
+        };
+        expect(actions.recipeSelected({})).to.eql(expectedAction)
+    });
+
+    it('should delete a recipe', () => {
+        const expectedAction = {
+            type: types.RECIPE_DELETE,
+            payload: {}
+        };
+        expect(actions.recipeDelete({})).to.eql(expectedAction)
+    });
+})
--- test/containers/RecipesPage/store/recipes_reducer_test.js
+++ test/containers/RecipesPage/store/recipes_reducer_test.js
@@ -1,126 +1,80 @@
-// import { expect } from '../../test_helper';
-//
-// import reducer from '../../../src/components/RecipesPage/store/recipe.reducer';
-// import {RECIPES_INITIAL_STATE} from '../../../src/components/RecipesPage/store/recipes_initial_state';
-// import * as types from '../../../src/components/RecipesPage/store/recipe.types';
-//
-//
-// describe('Reducer - Recipes', () => {
-//
-//   it('should return the initial state', () => {
-//     expect(
-//       reducer(undefined, {})
-//     ).to.eql(RECIPES_INITIAL_STATE)
-//   });
-//
-//
-//   it('should handle UPDATE_RECIPES', () => {
-//     expect(reducer([], {
-//       type: types.UPDATE_RECIPES,
-//       payload: []
-//     }))
-//     .to.eql({RecipesPage: []})
-//
-//     expect(reducer([], {
-//       type: types.UPDATE_RECIPES,
-//       payload: []
-//     })).to.eql({RecipesPage: []})
-//   });
-//
-//
-//   it('should handle SAVE_NEW_RECIPE', () => {
-//     expect(reducer([], {
-//       type: types.SAVE_NEW_RECIPE,
-//       payload: []
-//     }))
-//     .to.eql({RecipesPage: []})
-//
-//     expect(reducer([], {
-//       type: types.SAVE_NEW_RECIPE,
-//       payload: []
-//     })).to.eql({RecipesPage: []})
-//   });
-//
-//
-//   it('should handle DELETE_RECIPE', () => {
-//     expect(reducer([], {
-//       type: types.DELETE_RECIPE,
-//       payload: []
-//     }))
-//     .to.eql({RecipesPage: []})
-//
-//     expect(reducer([], {
-//       type: types.DELETE_RECIPE,
-//       payload: []
-//     })).to.eql({RecipesPage: []})
-//   });
-//
-//   it('should handle FETCH_RECIPE', () => {
-//     // expect(reducer(undefined, {
-//     //   type: types.FETCH_RECIPE,
-//     //   payload: 1
-//     // }))
-//     // .to.eql({recipe: {}})
-//
-//     // expect(reducer([], {
-//     //   type: types.FETCH_RECIPE,
-//     //   payload: []
-//     // })).to.eql({RecipesPage: []})
-//   });
-//
-//   it('should handle FETCH_NEW_RECIPE', () => {
-//     // expect(reducer([], {
-//     //   type: types.FETCH_NEW_RECIPE,
-//     //   payload: []
-//     // }))
-//     // .to.eql({RecipesPage: []})
-//     //
-//     // expect(reducer([], {
-//     //   type: types.FETCH_NEW_RECIPE,
-//     //   payload: []
-//     // })).to.eql({RecipesPage: []})
-//   });
-//
-//   it('should handle CLEAR_RECIPE', () => {
-//     // expect(reducer([], {
-//     //   type: types.CLEAR_RECIPE,
-//     //   payload: []
-//     // }))
-//     // .to.eql({RecipesPage: []})
-//     //
-//     // expect(reducer([], {
-//     //   type: types.CLEAR_RECIPE,
-//     //   payload: []
-//     // })).to.eql({RecipesPage: []})
-//   });
-//
-//   it('should handle DELETE_INGREDIENT', () => {
-//     // expect(reducer([], {
-//     //   type: types.DELETE_INGREDIENT,
-//     //   payload: []
-//     // }))
-//     // .to.eql({RecipesPage: []})
-//     //
-//     // expect(reducer([], {
-//     //   type: types.DELETE_INGREDIENT,
-//     //   payload: []
-//     // })).to.eql({RecipesPage: []})
-//   });
-//
-//
-//   it('should handle ADD_INGREDIENT', () => {
-//     // expect(reducer([], {
-//     //   type: types.ADD_INGREDIENT,
-//     //   payload: []
-//     // }))
-//     // .to.eql({RecipesPage: []})
-//     //
-//     // expect(reducer([], {
-//     //   type: types.ADD_INGREDIENT,
-//     //   payload: []
-//     // })).to.eql({RecipesPage: []})
-//   });
-//
-//
-//
-// })
+import { expect } from '../../../test_helper';
+import * as types from '../../../../src/containers/RecipesPage/store/recipe.types';
+
+import {
+    RECIPES_INITIAL_STATE
+} from '../../../../src/containers/RecipesPage/store/recipes_initial_state';
+import RecipeReducer from '../../../../src/containers/RecipesPage/store/recipe.reducer';
+
+
+describe('Recipe Reducer', () => {
+
+    const recipe = {
+        name: 'test',
+        description: 'test',
+        img: 'test',
+        ingredients: [],
+    };
+
+
+    it('should return the initial state', () => {
+        expect(RecipeReducer(undefined, {}))
+            .to
+            .eql(RECIPES_INITIAL_STATE);
+    });
+
+
+    it('should add a new recipe', () => {
+        const index = RECIPES_INITIAL_STATE.list.length;
+        let recipesState = Object.assign({}, RECIPES_INITIAL_STATE);
+        recipesState.list.push(recipe);
+
+        expect(RecipeReducer(RECIPES_INITIAL_STATE, {
+                type: types.RECIPE_NEW,
+                payload: recipe,
+            }
+        ).list[index]).to.deep.include(recipesState.list[index]);
+    });
+
+
+    it('should delete a recipe', () => {
+        const list = [ ...RECIPES_INITIAL_STATE.list ]
+        let recipesState = { ...RECIPES_INITIAL_STATE, list: list };
+        recipesState.list.splice(0, 1);
+
+        expect(RecipeReducer(RECIPES_INITIAL_STATE, {
+                type: types.RECIPE_DELETE,
+                payload: 0,
+            }
+        ).list.length).to.equal(recipesState.list.length);
+    });
+
+    it('should update a recipe', () => {
+        const index = 0;
+        let list = [ ...RECIPES_INITIAL_STATE.list ];
+        list[index] = recipe;
+        let recipesState = { ...RECIPES_INITIAL_STATE, list: list };
+
+        expect(RecipeReducer(RECIPES_INITIAL_STATE, {
+                type: types.RECIPE_UPDATE,
+                payload: {
+                    values: recipe,
+                    id: index,
+                },
+            }
+        ).list[index]).to.deep.include(recipesState.list[index]);
+    });
+
+
+    it('should select a recipe', () => {
+        const index = 0;
+        let list = [ ...RECIPES_INITIAL_STATE.list ];
+        let recipesState = { ...RECIPES_INITIAL_STATE, selected: list[index] };
+
+        expect(RecipeReducer(RECIPES_INITIAL_STATE, {
+                type: types.RECIPE_SELECTED,
+                payload: index,
+            }
+        ).selected).to.deep.include(recipesState.selected);
+    });
+});
